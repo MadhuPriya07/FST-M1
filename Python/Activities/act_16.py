4553dac5cd5c41238c867bf9ae60fba02fb16e30
MadhuPriya07 <mpmadhupriya280@gmail.com>
2021-12-06 22:59:40 +0530

    Add files via upload

--- Python/Activities/act_16.py
+++ Python/Activities/act_16.py
@@ -1,1 +1,18 @@
-ada
+class Car:
+    'This class represents a car'
+    def __init__(self, manufacture, model):
+        self.manufacture=manufacture
+        self.model=model
+    def accelerate(self):
+        print(self.manufacture, self.model, " is moving")
+    def stop(self):
+        print(self.manufacture, self.model, " has stopped moving")
+
+car1 = Car("Toyota", "Corolla")
+	
+car2 = Car("Maruti", "800")
+	
+car3 = Car("Suzuki", "Swift")
+
+car1.accelerate()
+car3.stop()
